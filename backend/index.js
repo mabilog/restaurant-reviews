0e38092aa59433cf3e60c6c7983cf1d842c80050
mabilog <60443712+angelo-mab@users.noreply.github.com>
2021-09-14 15:29:28 -0400

    spelling error in the backend. need to reread

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import app from "./server.js";
 import mongodb from 'mongodb'
 import dotenv from 'dotenv'
+import RestaurantsDAO from './dao/restaurantsDAO'
 
 dotenv.config();
 
@@ -11,12 +12,13 @@ const port = process.env.PORT || 8000
 MongoClient.connect(
   process.env.RESTREVIEWS_DB_URI,
   {
-    poolSize: 50,
-    writeConcern:{
-      wtimeout: 2500
-    },
-    useNewUrlParse: true,
-    useUnifiedTopology: true
+    maxPoolSize: 50,
+    // writeConcern:{
+    //   wtimeout: 2500
+    // },
+    wtimeoutMS: 2500,
+    useNewUrlParser: true,
+    // useUnifiedTopology: true
   }
 )
 .catch (err => {
@@ -24,6 +26,7 @@ MongoClient.connect(
   process.exit(1)
 })
 .then(async client => {
+  await RestaurantsDAO.injectDB(client)
   app.listen(port, () => 
   console.log(`listening on port ${port}`))
 })
